6efd72697c0682a3df3e561984bc3b584aec3254
KsRyY <andy200511@126.com>
2020-02-18 17:16:11 +0800

    :bug: Fix resource 404 when deployed to Github Pages

--- poi.config.js
+++ poi.config.js
@@ -1,8 +1,7 @@
+const isCI = require('is-ci')
+
 module.exports = {
-  entry: [
-    'src/registerServiceWorker',
-    'src/index'
-  ],
+  entry: ['src/registerServiceWorker', 'src/index'],
   plugins: [
     {
       resolve: '@poi/plugin-eslint'
@@ -10,5 +9,8 @@ module.exports = {
     {
       resolve: '@poi/plugin-pwa'
     }
-  ]
+  ],
+  output: {
+    publicUrl: isCI ? '/magic-typer/' : '/'
+  }
 }
